260e14d571d4b0fc766b55ea9dbdb70a2c4fc718
Elisson Silva <nossilesilva@gmail.com>
2022-05-01 00:47:33 -0300

    feat: created ingestion

--- app/ingestion.py
+++ app/ingestion.py
@@ -1,5 +1,16 @@
-from ingestion_data.ingestion import request_data
+import time
+import datetime
+
+from shared.jobs_loader import JobsLoader
+from shared.jobs_manager import JobsManager
 
 if __name__ == "__main__":
-    request_data()
+    s = '12/12/2022'
+    JobsLoader.Instance().load_jobs()
+    jobs_loader = JobsLoader.Instance()
+    jobs_loader.set_range(time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple()))
+    loaded_jobs = jobs_loader.loaded_jobs
 
+    job_manager = JobsManager(loaded_jobs)
+    results = job_manager.run()
+    print(results)
