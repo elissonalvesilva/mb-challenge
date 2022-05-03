53f334f0a6947b2247f2ef62b16e0bde5cdc440f
Elisson Silva <nossilesilva@gmail.com>
2022-05-03 20:59:38 -0300

    feat: load data in mongodb

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,11 +1,17 @@
 from decouple import config
+from urllib.parse import quote_plus
 
 __all__ = [
     'DEFINITION_FILE',
     'WORK_DIRECTORY_TO_RESULTS',
+    'get_mongodb_uri',
+    'MONGO_URI'
 ]
+
 # DEFINITION FILE DIRECTORIES
 PROJECT_FOLDER = config('PROJECT_FOLDER') + "/"
 DEFINITION_FILE = PROJECT_FOLDER + config('MB_PATH_JOB_DEFINITIONS')
 WORKDIRECTORY_TO_RESULTS = config('WORKDIRECTORY_TO_RESULTS') + '/{date}'
 
+#DATABASE ENV
+MONGO_URI = config('MONGO_URI')
--- app/database/loader.py
+++ app/database/loader.py
@@ -1,3 +1,12 @@
+from pymongo import MongoClient
+from config.settings import MONGO_URI
+
 class Loader():
     def __init__(self, data) -> None:
+        client = MongoClient(MONGO_URI)
+        self.client = client
         self.data = data
+
+    def load_data(self):
+        self.client.mb.sma.insert_many(self.data)
+
