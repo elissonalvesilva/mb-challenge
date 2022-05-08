4b7cb2c89556c58f195e2c89a58c24a072cff858
Elisson Silva <nossilesilva@gmail.com>
2022-05-08 18:51:08 -0300

    feat: added new envs

--- api/config/settings.py
+++ api/config/settings.py
@@ -3,3 +3,7 @@ from decouple import config
 class Settings:
     #DATABASE ENV
     MONGO_URI = config('MONGO_URI')
+
+    # APPLICATION ENV
+    API_PORT = config('API_PORT')
+    API_HOST = config('API_HOST')
